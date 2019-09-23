9a06d430ee6b4a6f683b850622f1b09551e03bac
dohyun <ehgus1551@naver.com>
2019-09-23 19:40:37 +0900

    settings

--- app/src/main/java/com/example/safebusfinalproject/MainActivity.java
+++ app/src/main/java/com/example/safebusfinalproject/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.safebusfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText editText = (EditText)findViewById(R.id.keywordEditText);
+        Button settingBtn = (Button)findViewById(R.id.settingBtn);
+
+        // (Android의 전형적인 event처리방식)
+        settingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 버튼을 눌렀을 때 서비스를 생성하고 실행.
+
+                Intent i = new Intent(MainActivity.this, ActSettings.class);
+                startActivity(i);
+
+            }
+        });
+
+
+        //버튼 ->  리스너
+
+
     }
+
+
 }
