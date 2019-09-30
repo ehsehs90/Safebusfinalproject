30f953f795a173523d5bc943fc68333cbd355556
shj <gurwls1240@naver.com>
2019-09-30 17:33:36 +0900

    회원가입구현완료 객체생성필요.

--- app/src/main/java/com/example/safebusfinalproject/Login3Activity.java
+++ app/src/main/java/com/example/safebusfinalproject/Login3Activity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -15,16 +16,22 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
-public class Login3Activity extends AppCompatActivity {
+public class Login3Activity extends AppCompatActivity{
+
+    long mNow;
+    Date mDate;
+    SimpleDateFormat mFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm");
 
     TextView text1,text2;
-    Switch switchAgree;
-    RadioGroup rGroup1, rGroupuser;
+    RadioGroup rGroup1, rGroupuser, rGroupgender;
     RadioButton Rdo50, Rdo60, Rdo70, driver, teacher, parents;
-    Button btnFinish, btnInit;
-    ImageView imgPet;
+    Button btnRegister;
+    ImageView imgPet,driverimage;
+    EditText babyName,name,phonenum,idet,pwet,address,license,carNum;
 
     RadioButton userArray[] = new RadioButton[3];
     RadioButton rArray[] = new RadioButton[3];
@@ -37,6 +44,9 @@ public class Login3Activity extends AppCompatActivity {
 
         // 위젯을 변수에 대입
 
+        idet = (EditText) findViewById(R.id.idText);
+        pwet = (EditText) findViewById(R.id.passwordText);
+
         text1 = (TextView) findViewById(R.id.Text1);
         text2 = (TextView) findViewById(R.id.Text2);
 
@@ -46,138 +56,130 @@ public class Login3Activity extends AppCompatActivity {
         rArray[1] = (RadioButton) findViewById(R.id.Rdo60);
         rArray[2] = (RadioButton) findViewById(R.id.Rdo70); //이미지
 
-        rGroupuser = (RadioGroup) findViewById(R.id.Rgroupuser);
+        //  rGroupuser = (RadioGroup) findViewById(R.id.Rgroupuser);
 
         userArray[0] = (RadioButton) findViewById(R.id.driver);
         userArray[1] = (RadioButton) findViewById(R.id.teacher);
         userArray[2] = (RadioButton) findViewById(R.id.parents);
 
-        btnFinish = (Button) findViewById(R.id.BtnFinish);
-        btnInit = (Button) findViewById(R.id.BtnInit);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+
         imgPet = (ImageView) findViewById(R.id.ImgPet);
+        driverimage = (ImageView) findViewById(R.id.driverimage);
 
-//        final int draw[] = {R.drawable.stationa, R.drawable.stationb, R.drawable.stationc};
+        name = (EditText) findViewById(R.id.name);
+        phonenum = (EditText) findViewById(R.id.phoneNum);
+        babyName = (EditText) findViewById(R.id.babyName);
+        address = (EditText) findViewById(R.id.address);
+        license = (EditText) findViewById(R.id.license);
+        carNum = (EditText) findViewById(R.id.carNum);
 
-//        final RadioGroup rg = (RadioGroup) findViewById(R.id.Rgroupuser);
-//        Button b = (Button)findViewById(R.id.button1);
-//        final TextView tv = (TextView)findViewById(R.id.textView2);
-//
-//        b.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                int id = rg.getCheckedRadioButtonId();
-//                //getCheckedRadioButtonId() 의 리턴값은 선택된 RadioButton 의 id 값.
-//                RadioButton rb = (RadioButton) findViewById(id);
-//                tv.setText("결과: " + rb.getText().toString());
-//
-//                String struser = rb.getText().toString();
-//
-//                Log.i("user",struser);
-//
-//                if(struser == "운전기사" && rb.isChecked() == true){
-//                    text2.setVisibility(android.view.View.VISIBLE);
-//                    rGroup1.setVisibility(android.view.View.VISIBLE);
-//                    btnFinish.setVisibility(android.view.View.VISIBLE);
-//                    btnInit.setVisibility(android.view.View.VISIBLE);
-//                    imgPet.setVisibility(android.view.View.VISIBLE);
-//                }
-//                else if(struser == "보육교사"){
-//                    text2.setVisibility(android.view.View.VISIBLE);
-//                    rGroup1.setVisibility(android.view.View.VISIBLE);
-//                    btnFinish.setVisibility(android.view.View.VISIBLE);
-//                    btnInit.setVisibility(android.view.View.VISIBLE);
-//                    imgPet.setVisibility(android.view.View.INVISIBLE);
-//                }
-//                else{
-//                    text2.setVisibility(android.view.View.VISIBLE);
-//                    rGroup1.setVisibility(android.view.View.VISIBLE);
-//                    btnFinish.setVisibility(android.view.View.INVISIBLE);
-//                    btnInit.setVisibility(android.view.View.INVISIBLE);
-//                    imgPet.setVisibility(android.view.View.INVISIBLE);
-//                }
-//
-//                // 라디오버튼 클릭시, 이벤트 처리
-//                for (int i = 0; i < draw.length; i++) {
-//                    final int index = i;
-//                    rArray[index].setOnClickListener(new View.OnClickListener() {
-//                        @Override
-//                        public void onClick(View view) {
-//                            imgPet.setImageResource(draw[index]);
-//                        }
-//                    });
-//
-//                    // 처음부터 다시 버튼을 클릭하면,
-//                    btnInit.setOnClickListener(new View.OnClickListener() {
-//                        @Override
-//                        public void onClick(View view) {
-//                            text2.setVisibility(android.view.View.INVISIBLE);
-//                            rGroup1.setVisibility(android.view.View.INVISIBLE);
-//                            btnFinish.setVisibility(android.view.View.INVISIBLE);
-//                            btnInit.setVisibility(android.view.View.INVISIBLE);
-//                            imgPet.setVisibility(android.view.View.INVISIBLE);
-//
-//                            // 라디오 그룹 초기화
-//                            rGroup1.clearCheck();
-//
-//                            // 스위치를 off로
-//                            //switchAgree.setChecked(false);
-//                        }
-//                    });
-//                }
-//
-//            } // end onClick()
-//        });  // end Listener
+        rGroupgender = (RadioGroup) findViewById(R.id.Rgroupgender);
 
+        rGroupgender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int id) {
+                RadioButton select = (RadioButton) findViewById(id);
 
+                String strgender = select.getText().toString();
+                String[] gender = {"남아", "여아"};
 
-        RadioGroup rg = (RadioGroup)findViewById(R.id.Rgroupuser);
+                Log.i("성별",strgender);
+
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                try {
+                    String result;
+                    String id = idet.getText().toString();
+                    String pw = pwet.getText().toString();
+
+                    RegisterActivity task = new RegisterActivity();
+                    result = task.execute(id, pw).get();
+                } catch (Exception e) {
+                    Log.i("DBtest", ".....ERROR.....!");
+                }
+            }
+        });
+
+        RadioGroup rg = (RadioGroup) findViewById(R.id.Rgroupuser);
 
         rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int id) {
-                RadioButton select = (RadioButton)findViewById(id);
+                RadioButton select = (RadioButton) findViewById(id);
 
                 String struser = select.getText().toString();
                 String[] userjob = {"운전기사", "보육교사", "학부모"};
 
 
-                Log.i("info1",struser);
-                Log.i("info2",userjob[0]);
+                Log.i("info1", struser);
+                Log.i("info2", userjob[0]);
 
-                if(select.isChecked() == true && struser==userjob[0]){
-                    Log.i("info3","if찍힘;");
+                if (select.isChecked() == true && struser.equals(userjob[0])) { // 운전기사
+                    //Log.i("info3","if찍힘;");
+                    text2.setVisibility(android.view.View.INVISIBLE);
+                    rGroup1.setVisibility(android.view.View.INVISIBLE);
+                    imgPet.setVisibility(android.view.View.INVISIBLE);
+                    rGroupgender.setVisibility(android.view.View.INVISIBLE);
+                    babyName.setVisibility(android.view.View.INVISIBLE);
+                    btnRegister.setVisibility(View.VISIBLE);
+                    address.setVisibility(View.INVISIBLE);
+                    license.setVisibility(View.VISIBLE);
+                    carNum.setVisibility(View.VISIBLE);
+                } else if (select.isChecked() == true && struser.equals(userjob[1])) { // 보육교사
+                    //Log.i("info3","else찍힘;");
+                    text2.setVisibility(android.view.View.INVISIBLE);
+                    rGroup1.setVisibility(android.view.View.INVISIBLE);
+                    imgPet.setVisibility(android.view.View.INVISIBLE);
+                    rGroupgender.setVisibility(android.view.View.INVISIBLE);
+                    babyName.setVisibility(android.view.View.INVISIBLE);
+                    btnRegister.setVisibility(View.VISIBLE);
+                    address.setVisibility(View.INVISIBLE);
+                    license.setVisibility(View.INVISIBLE);
+                    carNum.setVisibility(View.INVISIBLE);
+                } else if (select.isChecked() == true && struser.equals(userjob[2])) { // 학부모
+                    //Log.i("info3","else찍힘;");
+
+                    final int draw[] = {R.drawable.stationa, R.drawable.stationb, R.drawable.stationc};
+
+                    // 라디오버튼 클릭시, 이벤트 처리
+                    for (int i = 0; i < draw.length; i++) {
+                        final int index = i;
+                        rArray[index].setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                imgPet.setImageResource(draw[index]);
+                            }
+                        });
+                    }
+
                     text2.setVisibility(android.view.View.VISIBLE);
                     rGroup1.setVisibility(android.view.View.VISIBLE);
-                    btnFinish.setVisibility(android.view.View.INVISIBLE);
-                    btnInit.setVisibility(android.view.View.INVISIBLE);
                     imgPet.setVisibility(android.view.View.VISIBLE);
-                }
-                else{
-                    Log.i("info3","else찍힘;");
-                    text2.setVisibility(android.view.View.VISIBLE);
-                    rGroup1.setVisibility(android.view.View.VISIBLE);
-                    btnFinish.setVisibility(android.view.View.VISIBLE);
-                    btnInit.setVisibility(android.view.View.VISIBLE);
-                    imgPet.setVisibility(android.view.View.VISIBLE);
-                }
-
-
-                final int draw[] = {R.drawable.stationa, R.drawable.stationb, R.drawable.stationc};
-
-                // 라디오버튼 클릭시, 이벤트 처리
-                for (int i = 0; i < draw.length; i++) {
-                    final int index = i;
-                    rArray[index].setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            imgPet.setImageResource(draw[index]);
-                        }
-                    });
+                    rGroupgender.setVisibility(android.view.View.VISIBLE);
+                    babyName.setVisibility(android.view.View.VISIBLE);
+                    btnRegister.setVisibility(View.VISIBLE);
+                    address.setVisibility(View.VISIBLE);
+                    license.setVisibility(View.INVISIBLE);
+                    carNum.setVisibility(View.INVISIBLE);
                 }
 
             }
         });
 
+        String tmp = getTime();
+
+        Log.i("realtime", tmp);
+
+    }
+
+    private String getTime(){
+        mNow = System.currentTimeMillis();
+        mDate = new Date(mNow);
+        return mFormat.format(mDate);
     }
 
     public void onRadioButtonClicked (View v){
@@ -191,26 +193,6 @@ public class Login3Activity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), ((RadioButton) v).getText() + "unchecked",
                     Toast.LENGTH_SHORT).show();
         }
+
+
 }
-
-
-
-//        // 체크박스의 체크가 변경되면
-//        switchAgree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            public void onCheckedChanged(CompoundButton arg0, boolean arg1) {
-//                // 체크되면 모두 보이도록 설정
-//                if (switchAgree.isChecked() == true) {
-//                    text2.setVisibility(android.view.View.VISIBLE);
-//                    rGroup1.setVisibility(android.view.View.VISIBLE);
-//                    btnFinish.setVisibility(android.view.View.VISIBLE);
-//                    btnInit.setVisibility(android.view.View.VISIBLE);
-//                    imgPet.setVisibility(android.view.View.VISIBLE);
-//                } else {
-//                    text2.setVisibility(android.view.View.INVISIBLE);
-//                    rGroup1.setVisibility(android.view.View.INVISIBLE);
-//                    btnFinish.setVisibility(android.view.View.INVISIBLE);
-//                    btnInit.setVisibility(android.view.View.INVISIBLE);
-//                    imgPet.setVisibility(android.view.View.INVISIBLE);
-//                }
-//            }
-//        });
