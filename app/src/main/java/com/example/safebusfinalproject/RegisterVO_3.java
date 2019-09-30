b1d8a16437d89bda9e575b48ddac02c866f100f7
dohyun <ehgus1551@naver.com>
2019-09-30 11:32:12 +0900

    registerVO

---
+++ app/src/main/java/com/example/safebusfinalproject/RegisterVO_3.java
@@ -0,0 +1,103 @@
+package com.example.safebusfinalproject;
+
+import java.util.Date;
+
+public class RegisterVO_3 {
+    String memberID = null;
+    String memberPW = null;
+    String memberNum = null;
+    String driverName = null;
+    String driverTel = null;
+    String driverLicense = null;
+    String carNum = null;
+    String driverPicture = null;
+    Date registerDate = null;
+
+
+    public String getmemberID() {
+        return memberID;
+    }
+
+    public void setmemberID(String memberID) {
+        this.memberID = memberID;
+    }
+
+
+    public String getmemberPW() {
+        return memberPW;
+    }
+
+    public void setmemberPW(String memberPW) {
+        this.memberPW = memberPW;
+    }
+
+
+    public String getmemberNum() {
+        return memberNum;
+    }
+
+    public void setmemberNum(String memberNum) {
+        this.memberNum = memberNum;
+    }
+
+
+
+    public String getdriverName() {
+        return driverName;
+    }
+
+    public void setdriverName(String driverName) {
+        this.driverName = driverName;
+    }
+
+
+    public String getdriverTel() {
+        return driverTel;
+    }
+
+    public void setdriverTel(String driverTel) {
+        this.driverTel = driverTel;
+    }
+
+
+
+    public String getdriverLicense() {
+        return driverLicense;
+    }
+
+    public void setdriverLicense(String driverLicense) {
+        this.driverLicense = driverLicense;
+    }
+
+
+
+    public String getcarNum() {
+        return carNum;
+    }
+
+    public void setcarNum(String carNum) {
+        this.carNum = carNum;
+    }
+
+
+
+
+    public String getdriverPicture() {
+        return driverPicture;
+    }
+
+    public void setdriverPicture(String driverPicture) {
+        this.driverPicture = driverPicture;
+    }
+
+
+
+
+    public Date getregisterDate() {
+        return registerDate;
+    }
+
+    public void setregisterDate(Date registerDate) {
+        this.registerDate = registerDate;
+    }
+}
