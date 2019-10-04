6ae6c444c277c50a36e55fa25aab5eeaa974dbb6
shj <gurwls1240@naver.com>
2019-10-04 18:57:23 +0900

    버스vo

---
+++ app/src/main/java/com/example/safebusfinalproject/BusinfoVO.java
@@ -0,0 +1,69 @@
+package com.example.safebusfinalproject;
+
+public class BusinfoVO {
+
+    String temp = null;
+    String humidity = null;
+    String velocity = null;
+    String longitude = null; // 경도
+    String latitude = null; // 위도
+
+    public BusinfoVO() {
+    }
+
+    public BusinfoVO(String temp, String humidity, String velocity, String longitude, String latitude) {
+        super();
+        this.temp = temp;
+        this.humidity = humidity;
+        this.velocity = velocity;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    @Override
+    public String toString() {
+        return "BusinfoVO [temp=" + temp + ", humidity=" + humidity + ", velocity=" + velocity + ", longitude="
+                + longitude + ", latitude=" + latitude + "]";
+    }
+
+    public String getTemp() {
+        return temp;
+    }
+
+    public void setTemp(String temp) {
+        this.temp = temp;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(String humidity) {
+        this.humidity = humidity;
+    }
+
+    public String getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(String velocity) {
+        this.velocity = velocity;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+}
